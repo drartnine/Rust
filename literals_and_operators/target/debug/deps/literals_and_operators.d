b4eb184d450ded76e127087b8b2c637cbee77ba5
drartnine <98303071+drartnine@users.noreply.github.com>
2022-01-29 21:25:38 +0800

    Delete literals_and_operators directory

--- literals_and_operators/target/debug/deps/literals_and_operators.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Biolector2\Github2022\literals_and_operators\target\debug\deps\literals_and_operators.exe: src\main.rs
-
-C:\Users\Biolector2\Github2022\literals_and_operators\target\debug\deps\literals_and_operators.d: src\main.rs
-
-src\main.rs:
